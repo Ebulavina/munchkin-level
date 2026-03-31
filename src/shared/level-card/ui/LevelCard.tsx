05fd6ce7bf8060c2c7272790748cb1dce862e7c9
Ebulavina <e.s.buuulavina@gmail.com>
2026-03-31 14:11:20 +0200

    feat: add male/female

--- src/shared/level-card/ui/LevelCard.tsx
+++ src/shared/level-card/ui/LevelCard.tsx
@@ -4,24 +4,35 @@ import { MIN_LEVEL, MAX_LEVEL } from '../model/constants';
 
 export function LevelCard() {
   const [level, setLevel] = useState<number>(MIN_LEVEL);
+  const [isMale, setIsMale] = useState<boolean>(true);
 
   return (
     <div className={s.card}>
-      <button 
-        className={s.button} 
-        onClick={() => setLevel(level - 1)}      
-        disabled={level === MIN_LEVEL}
-      >
-        {'\u2212'}
-      </button>
-      <div className={s.number}>{level}</div>
-      <button 
-        className={s.button} 
-        onClick={() => setLevel(level + 1)} 
-        disabled={level === MAX_LEVEL}
-      >
-        {'\u002B'}
-      </button>
+      <div className={s.header}>
+        <button 
+          className={`${s.button} ${s.gender}`} 
+          onClick={() => setIsMale(!isMale)}
+        >
+          {isMale ? '\u2642' : '\u2640'}
+        </button>
+      </div>
+      <div className={s.content}>
+        <button 
+          className={s.button} 
+          onClick={() => setLevel(level - 1)}      
+          disabled={level === MIN_LEVEL}
+        >
+          {'\u2212'}
+        </button>
+        <div className={s.number}>{level}</div>
+        <button 
+          className={s.button} 
+          onClick={() => setLevel(level + 1)} 
+          disabled={level === MAX_LEVEL}
+        >
+          {'\u002B'}
+        </button>
+      </div>
     </div>
   );
 }
